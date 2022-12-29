567d323b633df541f2cc07513fdc43579b91b7cb
Yogi Wisesa <yogiwisesa@gmail.com>
2022-12-29 10:13:10 +0800

    chore: change filename case

--- src/components/Button/__tests__/Button.test.tsx
+++ src/components/Button/__tests__/Button.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react-native';
-import {Button} from '../';
+import {Button} from '..';
 
 describe('Button', () => {
   it('should render button correctly', () => {
